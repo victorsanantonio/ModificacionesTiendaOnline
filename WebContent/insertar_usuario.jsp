b1d1ad0bf41881d149024946f0cb77653a83a44e
victorsanantonio <victorsanantonio@hotmail.com>
2021-04-29 20:41:03 +0200

    Vista de insertar usuarios realizada

--- WebContent/insertar_usuario.jsp
+++ WebContent/insertar_usuario.jsp
@@ -19,6 +19,46 @@
 						<td>Id Rol</td>
 						<td><input type="text" name="id_rol" /></td>
 					</tr>
+					<tr>
+						<td>Email</td>
+						<td><input type="text" name="email" /></td>
+					</tr>
+					<tr>
+						<td>Clave</td>
+						<td><input type="text" name="clave" /></td>
+					</tr>
+					<tr>
+						<td>Nombre</td>
+						<td><input type="text" name="nombre" /></td>
+					</tr>
+					<tr>
+						<td>Primer apellido</td>
+						<td><input type="text" name="apellido1" /></td>
+					</tr>
+					<tr>
+						<td>Segundo apellido</td>
+						<td><input type="text" name="apellido2" /></td>
+					</tr>
+					<tr>
+						<td>Dirección</td>
+						<td><input type="text" name="direccion" /></td>
+					</tr>
+					<tr>
+						<td>Localidad</td>
+						<td><input type="text" name="localidad" /></td>
+					</tr>
+					<tr>
+						<td>Provincia</td>
+						<td><input type="text" name="provincia" /></td>
+					</tr>
+					<tr>
+						<td>Teléfono</td>
+						<td><input type="text" name="telefono" /></td>
+					</tr>
+					<tr>
+						<td>DNI</td>
+						<td><input type="text" name="dni" /></td>
+					</tr>
 				</table>
 				<input type="submit" value="Insertar"/>
 			</form>
--- src/vista/VistaInsertarUsuario.java
+++ src/vista/VistaInsertarUsuario.java
@@ -14,28 +14,48 @@ import controlador.crud.Insertar;
 public class VistaInsertarUsuario extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private Insertar insertar;
-	
+
 	public void init() {
 		insertar = new Insertar();
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		String id = request.getParameter("id");
-		String rol = request.getParameter("rol");
+		String id_rol = request.getParameter("id_rol");
+		String email = request.getParameter("email");
+		String clave = request.getParameter("clave");
+		String nombre = request.getParameter("nombre");
+		String apellido1 = request.getParameter("apellido1");
+		String apellido2 = request.getParameter("apellido2");
+		String direccion = request.getParameter("direccion");
+		String localidad = request.getParameter("localidad");
+		String provincia = request.getParameter("provincia");
+		String telefono = request.getParameter("telefono");
+		String dni = request.getParameter("dni");
 
-		response.sendRedirect("insertar_rol.jsp");
-		insertar.guardarRol(Integer.parseInt(id), rol);
+		response.sendRedirect("insertar_usuario.jsp");
+		insertar.guardarUsuario(Integer.parseInt(id), Integer.parseInt(id_rol), email, clave, nombre, apellido1,
+				apellido2, direccion, localidad, provincia, telefono, dni);
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
 		String id = request.getParameter("id");
-		String rol = request.getParameter("rol");
+		String id_rol = request.getParameter("id_rol");
+		String email = request.getParameter("email");
+		String clave = request.getParameter("clave");
+		String nombre = request.getParameter("nombre");
+		String apellido1 = request.getParameter("apellido1");
+		String apellido2 = request.getParameter("apellido2");
+		String direccion = request.getParameter("direccion");
+		String localidad = request.getParameter("localidad");
+		String provincia = request.getParameter("provincia");
+		String telefono = request.getParameter("telefono");
+		String dni = request.getParameter("dni");
 
-		response.sendRedirect("insertar_rol.jsp");
-		insertar.guardarRol(Integer.parseInt(id), rol);
+		response.sendRedirect("insertar_usuario.jsp");
+		insertar.guardarUsuario(Integer.parseInt(id), Integer.parseInt(id_rol), email, clave, nombre, apellido1,
+				apellido2, direccion, localidad, provincia, telefono, dni);
 	}
 }
